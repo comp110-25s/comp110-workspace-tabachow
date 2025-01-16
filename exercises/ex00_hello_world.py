ffd7846fc0963d2d23f88cb836bb03f9f636c64a
Isabel Tabachow <itabachow@unc.edu>
2025-01-16 15:49:26 -0500

    Finished Exercise00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,13 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730489389"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+if __name__ == "__main__":
+    print(greet(name=input("What is your name? ")))
+    
